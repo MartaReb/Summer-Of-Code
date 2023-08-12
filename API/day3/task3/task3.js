cc61793d6de37dfb5734bf93721b5146800ea29e
MartaReb <49365894+MartaReb@users.noreply.github.com>
2023-08-12 22:53:42 +0200

    add_task3

---
+++ API/day3/task3/task3.js
@@ -0,0 +1,12 @@
+fetch('https://reqres.in/api/users?page=1 ')
+    .then(res => res.json())
+    .then((data) => {
+        let users = data.data
+        for (let i=0; i < users.length; i++){
+            console.log("First name: " + users[i].first_name)
+            console.log("Last name: " + users[i].last_name)
+            console.log("Email: " + users[i].email)
+            console.log("Avatar: " + users[i].avatar)
+            console.log("--------")
+        }
+    }).catch(error => console.log('ERROR'))
